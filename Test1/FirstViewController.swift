38af68d7b4b58b9b4a6fd14044069b4e2d8f39c2
Zhuravlev Dmitry <qwerty-91@list.ru>
2022-02-07 12:50:20 +0500

    Create FirstViewController and SubView

---
+++ Test1/FirstViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FirstViewController.swift
+//  Test1
+//
+//  Created by Zdrenko Zigich on 07.02.2022.
+//
+
+import Foundation
+import UIKit
+
+final class FirstViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let subview = UIView()
+        view.addSubview(subview)
+    }
+}
